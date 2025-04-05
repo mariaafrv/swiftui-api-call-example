ad535ed3e44bd8f5ceac7356f99e5bd99960d6eb
Maria Aida Fernandes Vitoria <mariaafv@hotmail.com>
2025-04-05 12:40:04 -0300

    calling api

--- apiCallTest/ContentView.swift
+++ apiCallTest/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  apiCallTest
-//
-//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 05/04/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -16,6 +9,34 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .task {
+          // Define the endpoint url with parameters
+          
+          let urlString = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLyo6W3g1j4QZ2qB1uRi-x3A0xCcc1w-C1&key={YOUR_API_KEY}"
+          
+          // Create URL instance
+          let url = URL(string: urlString)
+          
+          if let url = url {
+            // Create URLRequest instance
+            let request = URLRequest(url: url)
+            
+            do {
+              // Send the request with URLSession
+              let (data, response) = try await URLSession.shared.data(for: request)
+              
+              // Print out the response
+              print(data)
+              print(response)
+            }
+            catch {
+              // Handle errors
+              print(error)
+            }
+
+          }
+          
+        }
     }
 }
 
--- apiCallTest/apiCallTestApp.swift
+++ apiCallTest/apiCallTestApp.swift
@@ -1,10 +1,3 @@
-//
-//  apiCallTestApp.swift
-//  apiCallTest
-//
-//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 05/04/25.
-//
-
 import SwiftUI
 
 @main
