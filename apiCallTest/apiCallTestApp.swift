69166811564e30e18d843830e8e8980da35f3c5e
Maria Aida Fernandes Vitoria <mariaafv@hotmail.com>
2025-04-05 11:31:56 -0300

    Initial Commit

---
+++ apiCallTest/apiCallTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiCallTestApp.swift
+//  apiCallTest
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct apiCallTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
